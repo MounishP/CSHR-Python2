07c7c0c35e8d134236c0dda76c1f3467f77a49ae
MounishP <mnsh.pv1@gmail.com>
2023-11-10 08:03:59 +0530

    Day 1

---
+++ Operators.py
@@ -0,0 +1,27 @@
+x = 5
+y = 7
+# Ctrl + /
+# arithmetic
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x // y)
+# print(x % y)
+# print(x ** y)
+
+#Comparion
+# Ctrl + D -> to copy the same to bottom
+# print(x > y)     #False
+# print(x < y)     #True
+# print(x == y)    #False
+# print(x != y)    #True
+# print(x >= y)    #False
+# print(x <= y)    #True
+
+#logical
+a = True
+b = False
+print(a and b)   #false
+print(a or b)    #true
+print(not a)     #false
---
+++ variable.py
@@ -0,0 +1,13 @@
+"""
+siadghsdf
+dsajksdb
+sdjkhbsv
+"""
+n = 34
+print(type(n))
+
+x = 5
+y = 34
+
+x += y
+print(x)
